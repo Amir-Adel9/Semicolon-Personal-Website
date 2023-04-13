d29b8df27904cc888df6b0eaa37ea5a5685d3714
Amir-Adel9 <amiradel7152@gmail.com>
2023-04-13 20:25:20 +0200

    Added basic Markup and Styles, and Theme Switcher

---
+++ logic/theme.js
@@ -0,0 +1,23 @@
+const themeSwitcher = document.querySelector('.theme_switcher');
+const selectedTheme = localStorage.getItem('theme');
+
+document.body.classList.add(selectedTheme);
+themeSwitcher.innerHTML = document.body.classList.contains('theme_dark')
+  ? 'Light Theme 🌞'
+  : 'Dark Theme 🌙';
+
+function toggleTheme() {
+  console.log('toggleTheme');
+  document.body.classList.toggle('theme_dark');
+  localStorage.setItem(
+    'theme',
+    document.body.classList.contains('theme_dark')
+      ? 'theme_dark'
+      : 'theme_light'
+  );
+  themeSwitcher.innerHTML = document.body.classList.contains('theme_dark')
+    ? 'Light Theme 🌞'
+    : 'Dark Theme 🌙';
+}
+
+themeSwitcher.addEventListener('click', toggleTheme);
