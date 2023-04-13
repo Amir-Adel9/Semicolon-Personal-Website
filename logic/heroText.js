154beef69872461715988cebfa468bec467cf63b
Amir-Adel9 <amiradel7152@gmail.com>
2023-04-14 00:43:48 +0200

    Hero changes, added side lines

---
+++ logic/heroText.js
@@ -0,0 +1,15 @@
+const wordToBeChanged = document.querySelector('.title_text_span');
+const wordsArray = ['self-taught', 'passionate', 'committed'];
+
+function changeWord() {
+  let i = 0;
+  setInterval(() => {
+    wordToBeChanged.innerHTML = wordsArray[i];
+    i++;
+    if (i === wordsArray.length) {
+      i = 0;
+    }
+  }, 1000);
+}
+
+changeWord();
